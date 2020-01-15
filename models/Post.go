5154a787e018362f00e8af67038ee9a5929b05fa
Hendi Santika <hendisantika@gmail.com>
2020-01-16 06:20:59 +0700

    #15 - Add Post model file

---
+++ models/Post.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+)
+
+type Post struct {
+	ID        uint64    `gorm:"primary_key;auto_increment" json:"id"`
+	Title     string    `gorm:"size:255;not null;unique" json:"title"`
+	Content   string    `gorm:"size:255;not null;" json:"content"`
+	Author    User      `json:"author"`
+	AuthorID  uint32    `gorm:"not null" json:"author_id"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
