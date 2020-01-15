1fd7b0669803df05fc9d859f6b2abb0b2980483e
Hendi Santika <hendisantika@gmail.com>
2020-01-16 06:17:39 +0700

    #6 - Add func VerifyPassword in User model file

--- models/User.go
+++ models/User.go
@@ -17,3 +17,7 @@ type User struct {
 func Hash(password string) ([]byte, error) {
 	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 }
+
+func VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
