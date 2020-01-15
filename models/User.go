a3c7eda42e97087858841a064e7ff59f70d1a354
Hendi Santika <hendisantika@gmail.com>
2020-01-16 06:19:38 +0700

    #12 - Add func FindUserByID in User model file

--- models/User.go
+++ models/User.go
@@ -107,3 +107,15 @@ func (u *User) FindAllUsers(db *gorm.DB) (*[]User, error) {
 	}
 	return &users, err
 }
+
+func (u *User) FindUserByID(db *gorm.DB, uid uint32) (*User, error) {
+	var err error
+	err = db.Debug().Model(User{}).Where("id = ?", uid).Take(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	if gorm.IsRecordNotFoundError(err) {
+		return &User{}, errors.New("User Not Found")
+	}
+	return u, err
+}
