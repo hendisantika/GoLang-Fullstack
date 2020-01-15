611b49d1596d02795086e2ba523dfa98af7427bf
Hendi Santika <hendisantika@gmail.com>
2020-01-16 06:17:07 +0700

    #4 - Add User model file

---
+++ models/User.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        uint32    `gorm:"primary_key;auto_increment" json:"id"`
+	Nickname  string    `gorm:"size:255;not null;unique" json:"nickname"`
+	Email     string    `gorm:"size:100;not null;unique" json:"email"`
+	Password  string    `gorm:"size:100;not null;" json:"password"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
