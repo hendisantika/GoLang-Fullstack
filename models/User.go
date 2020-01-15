7bfb20bf7e756bc3b76579d45dc1d8b6eb4bff19
Hendi Santika <hendisantika@gmail.com>
2020-01-16 06:17:55 +0700

    #7 - Add func BeforeSave in User model file

--- models/User.go
+++ models/User.go
@@ -21,3 +21,12 @@ func Hash(password string) ([]byte, error) {
 func VerifyPassword(hashedPassword, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 }
+
+func (u *User) BeforeSave() error {
+	hashedPassword, err := Hash(u.Password)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
