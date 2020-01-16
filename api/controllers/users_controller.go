6a3dff78ee5518d2d2edd8e00abf580776698b1e
Hendi Santika <hendisantika@gmail.com>
2020-01-17 06:40:36 +0700

    #37 - Add CreateUser in users_controller.go file

---
+++ api/controllers/users_controller.go
@@ -0,0 +1,42 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/victorsteven/fullstack/api/models"
+	"github.com/victorsteven/fullstack/api/responses"
+	"github.com/victorsteven/fullstack/api/utils/formaterror"
+	"io/ioutil"
+	"net/http"
+)
+
+func (server *Server) CreateUser(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+	}
+	user := models.User{}
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	user.Prepare()
+	err = user.Validate("")
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	userCreated, err := user.SaveUser(server.DB)
+
+	if err != nil {
+
+		formattedError := formaterror.FormatError(err.Error())
+
+		responses.ERROR(w, http.StatusInternalServerError, formattedError)
+		return
+	}
+	w.Header().Set("Location", fmt.Sprintf("%s%s/%d", r.Host, r.RequestURI, userCreated.ID))
+	responses.JSON(w, http.StatusCreated, userCreated)
+}
