00ab19a6d4024c197e1f9ceb37861388e4a1d28c
Hendi Santika <hendisantika@gmail.com>
2020-01-17 06:45:49 +0700

    #41 - Add DeleteUser in users_controller.go file

--- api/controllers/users_controller.go
+++ api/controllers/users_controller.go
@@ -114,3 +114,32 @@ func (server *Server) UpdateUser(w http.ResponseWriter, r *http.Request) {
 	}
 	responses.JSON(w, http.StatusOK, updatedUser)
 }
+
+func (server *Server) DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	user := models.User{}
+
+	uid, err := strconv.ParseUint(vars["id"], 10, 32)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+	tokenID, err := auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+	if tokenID != 0 && tokenID != uint32(uid) {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
+		return
+	}
+	_, err = user.DeleteAUser(server.DB, uint32(uid))
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+	w.Header().Set("Entity", fmt.Sprintf("%d", uid))
+	responses.JSON(w, http.StatusNoContent, "")
+}
