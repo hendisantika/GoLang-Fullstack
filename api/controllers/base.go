e30905edb91f5df5911b909275ba98386ca88e8e
Hendi Santika <hendisantika@gmail.com>
2020-01-17 06:38:28 +0700

    #33 - Add Server in base.go file

---
+++ api/controllers/base.go
@@ -0,0 +1,14 @@
+package controllers
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"    //mysql database driver
+	_ "github.com/jinzhu/gorm/dialects/postgres" //postgres database driver
+)
+
+type Server struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
