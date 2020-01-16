67bb935efbd4886ad72050db1ed87c643134a2d4
Hendi Santika <hendisantika@gmail.com>
2020-01-17 06:46:29 +0700

    #42 - Add CreatePost in posts_controller.go file

---
+++ api/controllers/posts_controller.go
@@ -0,0 +1,51 @@
+package controllers
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/victorsteven/fullstack/api/auth"
+	"github.com/victorsteven/fullstack/api/models"
+	"github.com/victorsteven/fullstack/api/responses"
+	"github.com/victorsteven/fullstack/api/utils/formaterror"
+	"io/ioutil"
+	"net/http"
+)
+
+func (server *Server) CreatePost(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	post := models.Post{}
+	err = json.Unmarshal(body, &post)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	post.Prepare()
+	err = post.Validate()
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	uid, err := auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+	if uid != post.AuthorID {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
+		return
+	}
+	postCreated, err := post.SavePost(server.DB)
+	if err != nil {
+		formattedError := formaterror.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, formattedError)
+		return
+	}
+	w.Header().Set("Lacation", fmt.Sprintf("%s%s/%d", r.Host, r.URL.Path, postCreated.ID))
+	responses.JSON(w, http.StatusCreated, postCreated)
+}
