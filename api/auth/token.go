cdcb9b83aa9c425ea1f6909aaaad12b6d35bd933
Hendi Santika <hendisantika@gmail.com>
2020-01-17 06:34:19 +0700

    #25 - Add CreateToken function in token.go file

---
+++ api/auth/token.go
@@ -0,0 +1,18 @@
+package auth
+
+import (
+	"os"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+func CreateToken(user_id uint32) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["user_id"] = user_id
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix() //Token expires after 1 hour
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(os.Getenv("API_SECRET")))
+
+}
