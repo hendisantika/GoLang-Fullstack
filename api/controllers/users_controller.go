d809abc2014b6b9ee3723e97f41371250a026dac
Hendi Santika <hendisantika@gmail.com>
2020-01-17 06:45:14 +0700

    #39 - Add GetUser in users_controller.go file

--- api/controllers/users_controller.go
+++ api/controllers/users_controller.go
@@ -8,6 +8,7 @@ import (
 	"github.com/victorsteven/fullstack/api/utils/formaterror"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 func (server *Server) CreateUser(w http.ResponseWriter, r *http.Request) {
@@ -51,4 +52,21 @@ func (server *Server) GetUsers(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	responses.JSON(w, http.StatusOK, users)
+}
+
+func (server *Server) GetUser(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	uid, err := strconv.ParseUint(vars["id"], 10, 32)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+	user := models.User{}
+	userGotten, err := user.FindUserByID(server.DB, uint32(uid))
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+	responses.JSON(w, http.StatusOK, userGotten)
 }
