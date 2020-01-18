c9ced3fcb565cb6c2ec7b4f93ef7e557dd0da649
Hendi Santika <hendisantika@gmail.com>
2020-01-18 20:36:32 +0700

    #43 - Add GetPosts in posts_controller.go file

--- api/controllers/posts_controller.go
+++ api/controllers/posts_controller.go
@@ -49,3 +49,15 @@ func (server *Server) CreatePost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Lacation", fmt.Sprintf("%s%s/%d", r.Host, r.URL.Path, postCreated.ID))
 	responses.JSON(w, http.StatusCreated, postCreated)
 }
+
+func (server *Server) GetPosts(w http.ResponseWriter, r *http.Request) {
+
+	post := models.Post{}
+
+	posts, err := post.FindAllPosts(server.DB)
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+	responses.JSON(w, http.StatusOK, posts)
+}
