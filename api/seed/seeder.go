9c199c1d18e9f9f41231af4605cc3aab96c94014
Hendi Santika <hendisantika@gmail.com>
2020-01-21 05:49:45 +0700

    #51 - Add posts in seeder.go file

--- api/seed/seeder.go
+++ api/seed/seeder.go
@@ -16,3 +16,14 @@ var users = []models.User{
 		Password: "password",
 	},
 }
+
+var posts = []models.Post{
+	models.Post{
+		Title:   "Title 1",
+		Content: "Hello world 1",
+	},
+	models.Post{
+		Title:   "Title 2",
+		Content: "Hello world 2",
+	},
+}
