e54e79cdadbd8a6b5043dccbe6590b5d20588d69
Hendi Santika <hendisantika@gmail.com>
2020-01-21 05:50:00 +0700

    #52 - Add Load in seeder.go file

--- api/seed/seeder.go
+++ api/seed/seeder.go
@@ -2,6 +2,7 @@ package seed
 
 import (
 	"github.com/victorsteven/fullstack/api/models"
+	"log"
 )
 
 var users = []models.User{
@@ -26,4 +27,34 @@ var posts = []models.Post{
 		Title:   "Title 2",
 		Content: "Hello world 2",
 	},
+}
+
+func Load(db *gorm.DB) {
+
+	err := db.Debug().DropTableIfExists(&models.Post{}, &models.User{}).Error
+	if err != nil {
+		log.Fatalf("cannot drop table: %v", err)
+	}
+	err = db.Debug().AutoMigrate(&models.User{}, &models.Post{}).Error
+	if err != nil {
+		log.Fatalf("cannot migrate table: %v", err)
+	}
+
+	err = db.Debug().Model(&models.Post{}).AddForeignKey("author_id", "users(id)", "cascade", "cascade").Error
+	if err != nil {
+		log.Fatalf("attaching foreign key error: %v", err)
+	}
+
+	for i := range users {
+		err = db.Debug().Model(&models.User{}).Create(&users[i]).Error
+		if err != nil {
+			log.Fatalf("cannot seed users table: %v", err)
+		}
+		posts[i].AuthorID = users[i].ID
+
+		err = db.Debug().Model(&models.Post{}).Create(&posts[i]).Error
+		if err != nil {
+			log.Fatalf("cannot seed posts table: %v", err)
+		}
+	}
 }
