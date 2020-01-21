ad3233fb6c1e53752a11fb5f231c196d16b5990b
Hendi Santika <hendisantika@gmail.com>
2020-01-21 07:39:53 +0700

    #54 - Add Run in server.go file

--- api/server.go
+++ api/server.go
@@ -1,7 +1,28 @@
 package api
 
 import (
+	"fmt"
 	"github.com/victorsteven/fullstack/api/controllers"
+	"log"
+	"os"
 )
 
 var server = controllers.Server{}
+
+func Run() {
+
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error getting env, not comming through %v", err)
+	} else {
+		fmt.Println("We are getting the env values")
+	}
+
+	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+
+	seed.Load(server.DB)
+
+	server.Run(":8080")
+
+}
